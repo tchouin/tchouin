aeb85cccea83ed9ba17309ea99777da58e3ddada
tchouin <48568504+tchouin@users.noreply.github.com>
2019-05-28 19:38:57 -0400

    Add files via upload

---
+++ js/scriptation.js
@@ -0,0 +1,51 @@
+$("#Img1").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img2").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img3").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img4").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img5").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img6").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img7").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img8").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+$("#Img9").mouseenter(function(){
+    $("#Img1").css('transform','scale(1)');
+})
+
+
+//jquery 2 dernieres pages
+$("#Img10").mouseenter(function(){
+    $("#Img10").css('transform','scale(1)');
+    $('#img10').attr("title", "Tornade de feu");
+    $('#t1').html("Espace");
+})
+$("#Img10").mouseleave(function(){
+
+    $('#t1').html("L'histoire de la lutte organisée contre les incendies commence au temps de l'Égypte et de la Grèce anciennes, où des gardes et rondes de nuit\n" +
+        "                étaient effectuées et où des pompes manuelles auraient\n" +
+        "                été employées pour éteindre des incendies. Cependant, de telles tentatives étaient d'une ampleur limitée lorsqu'elles sont comparées aux grandes\n" +
+        "                organisations qui se sont développées ensuite à Rome et dans de nombreuses autres villes. La première brigade de pompiers romains (les Vigiles urbani) a\n" +
+        "\n" +
+        "                été fondée avec pour mission de combattre le feu au moyen de seaux et de pompes pour transporter l'eau, mais également de divers outillages incluant\n" +
+        "                les siphonnes (pompes perfectionnées par Ctésibios vers -130, ancêtres de la pompe à bras) jusqu'aux catapultes permettant de détruire les maisons avant l'arrivée des flammes afin de contenir le foyer de l'incendie. Les cohortes urbaines sous l'empereur Auguste sont chargées de la prévention et de la surveillance.\n" +
+        "\n" +
+        "                Rome a subi de nombreux incendies d'une ampleur importante, notamment le plus connu qui commença aux abords du Circus Maximus\n" +
+        "                le 19 juillet 64 et détruisit plus des deux tiers de la ville de Rome. Le peuple qui cherchait un responsable pour ce désastre se tourna vers l'empereur Néron qui était suspecté de vouloir immortaliser son nom en renommant Rome Neropolis. Celui-ci rejeta finalement la responsabilité sur une petite communauté orientale, celle des chrétiens. On[Qui ?] raconte à propos de cet incendie qu'un Romain serait devenu très riche en achetant des propriétés lors de l'avancée des flammes et utilisant des équipes d'esclaves pour lutter contre l'incendie afin de préserver ses nouvelles acquisitions des flammes.\n" +
+        "\n" +
+        "                Il n'est pas connu de système de lutte contre l'incendie en Gaule4.");
+})
+
